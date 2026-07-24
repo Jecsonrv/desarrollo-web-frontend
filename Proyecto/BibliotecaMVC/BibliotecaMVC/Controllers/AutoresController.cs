a4e894e124bd2e4e837abf9869c16fd8af584115
Jecson <jecsonrv3@gmail.com>
2026-07-24 07:18:34 -0600

    Tarea semana 2, se añadio el controlador para autores, el modelo y la vista del mismo

---
+++ Proyecto/BibliotecaMVC/BibliotecaMVC/Controllers/AutoresController.cs
@@ -0,0 +1,61 @@
+﻿using BibliotecaMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BibliotecaMVC.Controllers
+{
+    public class AutoresController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Autor> autores = new List<Autor>()
+            {
+                new Autor
+                {
+                    ID = 1,
+                    Nombre = "Robert",
+                    Apellido = "Martin",
+                    Nacionalidad = "Estadounidense",
+                    FechaNacimiento = new DateOnly(1952, 12, 5),
+                    Activo = true
+                },
+                new Autor
+                {
+                    ID = 2,
+                    Nombre = "Martin",
+                    Apellido = "Fowler",
+                    Nacionalidad = "Británico",
+                    FechaNacimiento = new DateOnly(1963, 12, 18),
+                    Activo = true
+                },
+                new Autor
+                {
+                    ID = 3,
+                    Nombre = "Andrew",
+                    Apellido = "Hunt",
+                    Nacionalidad = "Estadounidense",
+                    FechaNacimiento = new DateOnly(1964, 1, 1), // Fecha aproximada (día/mes no ampliamente documentados)
+                    Activo = true
+                },
+                new Autor
+                {
+                    ID = 4,
+                    Nombre = "David",
+                    Apellido = "Thomas",
+                    Nacionalidad = "Británico",
+                    FechaNacimiento = new DateOnly(1956, 1, 1), // Fecha aproximada (día/mes no ampliamente documentados)
+                    Activo = true
+                },
+                new Autor
+                {
+                    ID = 5,
+                    Nombre = "R. J.",
+                    Apellido = "Palacio",
+                    Nacionalidad = "Estadounidense",
+                    FechaNacimiento = new DateOnly(1963, 7, 13),
+                    Activo = false
+                }
+            };
+            return View(autores);
+        }
+    }
+}
---
+++ Proyecto/BibliotecaMVC/BibliotecaMVC/Controllers/LibrosController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BibliotecaMVC.Models;
+
+namespace BibliotecaMVC.Controllers
+{
+    public class LibrosController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Libro> libros = new List<Libro>()
+            {
+                new Libro
+                {
+                    ID = 1,
+                    Titulo = "Clean Code",
+                    Autor = "Robert C. Martin",
+                    Categoria = "Programación",
+                    Precio = 29.99m,
+                    Disponible = true
+                },
+
+                new Libro{
+                    ID = 2,
+                    Titulo = "The Pragmatic Programmer",
+                    Autor = "Andrew Hunt, David Thomas",
+                    Categoria = "Programación",
+                    Precio = 34.99m,
+                    Disponible = false
+                },
+
+            };
+
+            //ViewBag.Libros = libros;
+
+            return View(libros);
+        }
+    }
+}
